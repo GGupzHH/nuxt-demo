a09d7c8e7a175e7a131e8deae00d5bb00858c2d1
GGupzHH <1248023357@qq.com>
2021-05-04 21:48:15 +0800

    feat(nuxt init)

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      123
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
